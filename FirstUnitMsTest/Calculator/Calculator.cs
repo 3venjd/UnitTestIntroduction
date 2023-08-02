75531f12b439ca6620712f1966bc10ac4e5d726f
luisj@sciww.com <luisj@sciww.com>
2023-08-01 22:33:17 -0500

    First MSTest

---
+++ FirstUnitMsTest/Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public static class Calculator
+    {
+        public static int Sum(int N1, int N2)
+        {
+            return N1 + N2;
+        }
+
+        public static int Substract(int N1, int N2)
+        {
+            return N1 - N2;
+        }
+
+        public static int Multiplication(int N1, int N2)
+        {
+            return N1 * N2;
+        }
+
+        public static int Division(int N1, int N2)
+        {
+            return N1 / N2;
+        }
+    }
+}
---
+++ FirstUnitMsTest/MsTest1/UnitTest1.cs
@@ -0,0 +1,74 @@
+using Calculator;
+
+namespace MsTest1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        //Arrange: Start the variables
+        int val1 = 6;
+        int val2 = 2;
+
+        [TestMethod]
+        public void Test_correct_sum()
+        {
+
+            //Act:execute the method to test
+            int resultado = Calculator.Calculator.Sum(val1, val2);
+
+
+            //Assert: check the values
+            Assert.AreEqual(8, resultado);
+            
+        }
+
+        [TestMethod]
+        public void Test_correct_Substract()
+        {
+
+            //Act:execute the method to test
+            int resultado = Calculator.Calculator.Substract(val1, val2);
+
+
+            //Assert: check the values
+            Assert.AreEqual(4, resultado);
+
+        }
+
+        [TestMethod]
+        public void Test_correct_Multiplication()
+        {
+
+            //Act:execute the method to test
+            int resultado = Calculator.Calculator.Multiplication(val1, val2);
+
+
+            //Assert: check the values
+            Assert.AreEqual(12, resultado);
+
+        }
+
+        [TestMethod]
+        public void Test_correct_Division()
+        {
+
+            //Act:execute the method to test
+            int resultado = Calculator.Calculator.Division(val1, val2);
+
+
+            //Assert: check the values
+            Assert.AreEqual(3, resultado);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void test_exceptions()
+        {
+            int dividend = 120;
+            int divider = 0;
+
+            double result = Calculator.Calculator.Division(dividend,divider);
+        }
+    }
+}
